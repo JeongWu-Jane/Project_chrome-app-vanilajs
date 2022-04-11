f2d98be7e77ece9104defa4ba8e6d6e1ccf87da5
JeongWu-Jane <park96100331@gmail.com>
2022-04-11 21:48:52 +0900

    20220323

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hi");
+const player = {
+  name: "Jane",
+  sayHello: function (otherPersonsName) {
+    console.log("Hello " + otherPersonsName + " Nice to meet you!");
+  },
+};
+player.sayHello("Nico");
